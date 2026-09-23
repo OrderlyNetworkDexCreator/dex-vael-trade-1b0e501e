3bf5458fb8ca8adfcfa98f43d206db5e60dd2d36
orderly-guoqing <guoqing@orderly.network>
2026-09-24 07:54:02 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -22,7 +22,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ENABLED_MENUS": "Leaderboard,Markets,Portfolio,Trading",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
-  "VITE_ENABLE_CAMPAIGNS": "true",
+  "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "https://x.com/VaelTrade",
